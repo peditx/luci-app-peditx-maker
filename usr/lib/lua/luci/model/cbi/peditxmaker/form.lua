75cb3e20537eb51ebba21ea3b89b40507984694c
PeDitX <pedram.ale@me.com>
2024-11-21 05:21:32 +0330

    Update form.lua

--- usr/lib/lua/luci/model/cbi/peditxmaker/form.lua
+++ usr/lib/lua/luci/model/cbi/peditxmaker/form.lua
@@ -1,7 +1,7 @@
-local m, s, btn_install
+local m, s, btn_install, btn_install_passwall
 
 -- Define the form
-m = Map("peditxmaker", "PeDitX-Maker", "A tool for installing PassWall2")
+m = Map("peditxmaker", "PeDitX-Maker", "A tool for installing PassWall2 and PassWall")
 
 -- Define a section
 s = m:section(TypedSection, "settings", "Settings")
@@ -14,6 +14,14 @@ btn_install.inputtitle = "Install PassWall2"
 btn_install.inputstyle = "apply"
 function btn_install.write(self, section)
     luci.sys.call("wget -O /tmp/passwall2.sh 'https://raw.githubusercontent.com/peditx/iranIPS/refs/heads/main/.files/passwall2.sh' && chmod +x /tmp/passwall2.sh && sh /tmp/passwall2.sh &")
+end
+
+-- Button for installing PassWall
+btn_install_passwall = s:option(Button, "_install_passwall", "Install PassWall")
+btn_install_passwall.inputtitle = "Install PassWall"
+btn_install_passwall.inputstyle = "apply"
+function btn_install_passwall.write(self, section)
+    luci.sys.call("wget -O /tmp/passwall.sh 'https://raw.githubusercontent.com/peditx/iranIPS/refs/heads/main/.files/passwall.sh' && chmod +x /tmp/passwall.sh && sh /tmp/passwall.sh &")
 end
 
 return m
